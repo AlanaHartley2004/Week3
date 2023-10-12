de30f2ed68c30b16d93a975d352272b3b247c78b
c3664188 <a.hartley7738@student.leedsbeckett.ac.uk>
2023-10-12 10:10:35 +0100

    Boolean

--- Week3.py
+++ Week3.py
@@ -1,17 +1,5 @@
-from typing import List, Any
-
-marks = []
-mark = input("Enter mark one")
-marks = marks +[mark]
-mark = input("Enter mark two")
-marks = marks +[mark]
-mark = input("Enter mark three")
-marks = marks +[mark]
-mark = input("Enter mark four")
-marks = marks +[mark]
-mark = input("Enter mark five")
-marks = marks +[mark]
-print("Marks are : ")
-print(marks)
-print("highest is ", max(marks))
-print("lowest is ", min(marks))
+print(50 >= 50)
+print(100 != 100)
+print("a" < "b")
+print("b" < "a")
+print("john" < "terry")
